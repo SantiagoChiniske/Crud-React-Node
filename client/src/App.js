44d18ef9fa2db9fb0c02d9c41ea5c5ddbeee9e4d
Santiago Chiniske Pereira <santiagochiniske10@gmail.com>
2023-10-23 20:58:08 -0300

    Parte front-end

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [values,setValues] = useState();
+
+  const handleChangeValues = (value) =>{
+    setValues((prevValue) =>({
+      ...prevValue,
+      [value.target.name]: value.target.value,
+
+    }));
+  };
+
+  const handleClickButton = () => {
+    console.log(values);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="app--container" >
+      
+      <div className='register--container'>
+        <h1 className='register--title'>Scrim Shop</h1>
+        <input type='text' name='name' placeholder='nome' className='register--input' onChange={handleChangeValues} />
+        <input type='text' name='cost' placeholder='preço' className='register--input' onChange={handleChangeValues}/>
+        <input type='text' name='category' placeholder='categoria' className='register--input' onChange={handleChangeValues}/>
+
+        <button className='register--button' onClick={()=> handleClickButton()}>Cadastrar</button>
+      </div>
+
+  </div >
   );
 }
 
